e5733d79a6c598b7305947ff35e7f502583c4b18
Joao Ventura <joao.ventura@buzzbrothers.ch>
2020-08-14 17:43:11 +0200

    start adding logic for positioning

--- snek.py
+++ snek.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 
 class SnekGame:
@@ -9,8 +10,9 @@ class SnekGame:
   game_bg = pygame.image.load("assets/background.png")
 
   # Attributes
-  snek_pos = (50,50)
-  food_pos = (100,100)
+  wall_thickness = 10 #px
+  snek_body_width = 20 #px
+
 
   # Constructor, called when class is instantiated
   def __init__(self):
@@ -18,10 +20,9 @@ class SnekGame:
     pygame.display.set_caption("Snek Game")
 
     self._running = True
-    self.size = self.weight, self.height = 422, 422
+    self.size = self.weight, self.height = 420, 420
     self._screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
     self._running = True
-
     self.game_loop()
 
   # Handles Events
@@ -29,19 +30,35 @@ class SnekGame:
     if event.type == pygame.QUIT:
         self._running = False
 
+  def first_update(self):
+    self.snek_pos = pygame.math.Vector2(10,10)
+
+    # Put the food somewhere at random
+    self.food_pos = pygame.math.Vector2(random.randint(0,20), random.randint(0,20))
+    while self.food_pos.x == 10 and self.food_pos.y == 10 :
+      self.food_pos = pygame.math.Vector2(random.randint(0,20), random.randint(0,20))
+
   # Game loop, handles logic
   def on_update(self):
     pass
 
-  # Called after on_loop , renders state to the screen
-  def on_draw(self):
-
+  def first_draw(self):
     self._screen.blit(self.game_bg, (0,0))
-    self._screen.blit(self.snek_food, self.food_pos)
-    self._screen.blit(self.snek_body, self.snek_pos)
+
+    starting_food_px = (self.food_pos * self.snek_body_width)
+    print("Starting food pos = " + str(self.food_pos) + " and resulting px = " + str(starting_food_px))
+    self._screen.blit(self.snek_food, starting_food_px )
+
+    starting_snek_px = (self.snek_pos * self.snek_body_width)
+    print("Starting Snek pos = " + str(self.food_pos) + " and resulting px = " + str(starting_snek_px))
+    self._screen.blit(self.snek_body, starting_snek_px)
     
     pygame.display.flip()
+
+  # Called after on_loop , renders state to the screen
+  def on_draw(self):
     pass
+
 
   # Called when game is closed
   def on_cleanup(self):
@@ -49,6 +66,8 @@ class SnekGame:
 
   # Calls the other methods
   def game_loop(self):
+    self.first_update()
+    self.first_draw()
     while( self._running ):
       for event in pygame.event.get():
         self.on_event(event)
