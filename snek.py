aaf2c30c6c9473b74b4e39606238d151fc956fb0
Joao Ventura <joao.ventura@buzzbrothers.ch>
2020-08-17 12:03:22 +0200

    Fix control issue. Clear screen each frame. Make game playable

--- snek.py
+++ snek.py
@@ -23,7 +23,7 @@ class SnekGame:
   def __init__(self):
     pygame.display.set_icon(self.snek_body)
     pygame.display.set_caption("Snek Game")
-
+    self._fps = 5
     self._clock = pygame.time.Clock()
     self._running = True
     self.size = self.weight, self.height = 420, 420
@@ -37,22 +37,34 @@ class SnekGame:
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_UP:
         self.arrows[0] = True
+        self.arrows[1] = False
+        self.arrows[2] = False
+        self.arrows[3] = False
       elif event.key == pygame.K_RIGHT:
         self.arrows[1] = True
+        self.arrows[0] = False
+        self.arrows[2] = False
+        self.arrows[3] = False
       elif event.key == pygame.K_DOWN:
         self.arrows[2] = True
+        self.arrows[0] = False
+        self.arrows[1] = False
+        self.arrows[3] = False
       elif event.key == pygame.K_LEFT:
         self.arrows[3] = True
-
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_UP:
         self.arrows[0] = False
-      elif event.key == pygame.K_RIGHT:
         self.arrows[1] = False
-      elif event.key == pygame.K_DOWN:
         self.arrows[2] = False
-      elif event.key == pygame.K_LEFT:
-        self.arrows[3] = False
+
+    # if event.type == pygame.KEYUP:
+    #   if event.key == pygame.K_UP:
+    #     self.arrows[0] = False
+    #   elif event.key == pygame.K_RIGHT:
+    #     self.arrows[1] = False
+    #   elif event.key == pygame.K_DOWN:
+    #     self.arrows[2] = False
+    #   elif event.key == pygame.K_LEFT:
+    #     self.arrows[3] = False
     
 
   def first_update(self):
@@ -76,22 +88,25 @@ class SnekGame:
     # Update position of various body parts
     if self.cur_direction >= 0 :
       for x in range(len(self.snek_pos), 0, -1):
-        if x - 1 == 0:
-          if self.cur_direction == 0 :
-            self.snek_pos[0] += pygame.math.Vector2(0,-1)
-          elif self.cur_direction == 1 :
-            self.snek_pos[0] += pygame.math.Vector2(1,0)
-          elif self.cur_direction == 2 :
-            self.snek_pos[0] += pygame.math.Vector2(0,1)
-          elif self.cur_direction == 3 :
-            self.snek_pos[0] += pygame.math.Vector2(-1,0)
-        else:
-          self.snek_pos[x - 1] = self.snek_pos[x - 2]
+        if(x > 1):
+          self.snek_pos[x - 1].x = self.snek_pos[x - 2].x
+          self.snek_pos[x - 1].y = self.snek_pos[x - 2].y
+
+    if self.cur_direction == 0 :
+      self.snek_pos[0] += pygame.math.Vector2(0,-1)
+    elif self.cur_direction == 1 :
+      self.snek_pos[0] += pygame.math.Vector2(1,0)
+    elif self.cur_direction == 2 :
+      self.snek_pos[0] += pygame.math.Vector2(0,1)
+    elif self.cur_direction == 3 :
+      self.snek_pos[0] += pygame.math.Vector2(-1,0)
+    
+    self.check_for_collisions()
 
     if self.snek_pos[0] == self.food_pos:
       self.regenerate_food()
       self.make_snek_bigger()
-    print(self.snek_pos)
+
 
   def first_draw(self):
     self._screen.blit(self.game_bg, (0,0))
@@ -108,6 +123,7 @@ class SnekGame:
 
   # Called after on_loop , renders state to the screen
   def on_draw(self):
+    self._screen.fill( (0,0,0) )
     self._screen.blit(self.game_bg, (0,0))
     pygame.display.update()
     
@@ -118,7 +134,7 @@ class SnekGame:
       self._screen.blit(self.snek_body, cur_px)
 
     pygame.display.update()
-    self._clock.tick(10)
+    self._clock.tick(self._fps)
 
   def make_snek_bigger(self):
     snek_len = len(self.snek_pos)
@@ -126,12 +142,26 @@ class SnekGame:
       self.snek_pos[snek_len - 1][0],
       self.snek_pos[snek_len - 1][1]
     ))
+    self._fps += 1
 
   def regenerate_food(self):
     self.food_pos = pygame.math.Vector2(random.randint(1,19), random.randint(1,19))
     while self.food_pos.x == self.snek_pos[0].x and self.food_pos.y == self.snek_pos[0].y :
       self.food_pos = pygame.math.Vector2(random.randint(1,19), random.randint(1,19))
-    
+
+  def check_for_collisions(self):
+    if(self.snek_pos[0].x == 0 or self.snek_pos[0].x == 20 or self.snek_pos[0].y == 0 or self.snek_pos[0].y == 20):
+      self.game_over()
+
+    for x in range(len(self.snek_pos), 0, -1):
+      if x > 2 :
+        if self.snek_pos[0].x == self.snek_pos[x - 1].x and self.snek_pos[0].y == self.snek_pos[x - 1].y:
+          self.game_over()
+
+  def game_over(self):
+    print("game over")
+    self._running = False
+
   # Called when game is closed
   def on_cleanup(self):
     pygame.quit()
